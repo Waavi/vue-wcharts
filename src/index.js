a8a195f64f44596f2c8328e0edc1dd3b743ab0df
alejandroledesma <alexledesmarayo@gmail.com>
2019-05-06 14:42:42 +0200

    Added install default in index.js and update main in package json

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
-export { WCartesian } from './charts'
-export {
-    WLine, WBar, WXAxis, WYAxis, WLegend, WCartesianGrid, WMarker, WTooltip,
+import { WCartesian } from './charts'
+import {
+    WLine,
+    WBar,
+    WXAxis,
+    WYAxis,
+    WLegend,
+    WCartesianGrid,
+    WMarker,
+    WTooltip,
 } from './components'
+
+function install (Vue) {
+    Vue.component('WCartesian', WCartesian)
+    Vue.component('WLine', WLine)
+    Vue.component('WBar', WBar)
+    Vue.component('WXAxis', WXAxis)
+    Vue.component('WYAxis', WYAxis)
+    Vue.component('WTooltip', WTooltip)
+    Vue.component('WLegend', WLegend)
+}
+
+export default install
+
+export {
+    WCartesian,
+    WLine,
+    WXAxis,
+    WYAxis,
+    WLegend,
+    WCartesianGrid,
+    WMarker,
+    WTooltip,
+}
