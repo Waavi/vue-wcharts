1c9b31c00b9e6db065266951107ccb178cb8de43
alejandroledesma <alexledesmarayo@gmail.com>
2019-05-07 08:36:14 +0200

    Refactor register component in install

--- src/index.js
+++ src/index.js
@@ -11,15 +11,19 @@ import {
 } from './components'
 
 function install (Vue) {
-    Vue.component('WCartesian', WCartesian)
-    Vue.component('WLine', WLine)
-    Vue.component('WBar', WBar)
-    Vue.component('WXAxis', WXAxis)
-    Vue.component('WYAxis', WYAxis)
-    Vue.component('WLegend', WLegend)
-    Vue.component('WCartesianGrid', WCartesianGrid)
-    Vue.component('WMarker', WMarker)
-    Vue.component('WTooltip', WTooltip)
+    [
+        WCartesian,
+        WLine,
+        WBar,
+        WXAxis,
+        WYAxis,
+        WLegend,
+        WCartesianGrid,
+        WMarker,
+        WTooltip,
+    ].forEach((c) => {
+        Vue.component(c.name, c)
+    })
 }
 
 export default install
