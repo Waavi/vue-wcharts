b1050bac3ecec666e4de36a5683a777413205189
Carlos Cepeda <carlos@cepeda.me>
2019-05-30 18:27:17 +0200

    Add stacked value on top

--- src/components/Bar/WBar.vue
+++ src/components/Bar/WBar.vue
@@ -25,21 +25,62 @@
                     :fill="fill"
                     :style="{ styles, transition }"
                 />
-                <slot
-                    name="label"
-                    v-bind="bar.label"
+                <g
+                    v-if="bar.label"
                 >
+                    <slot
+                        name="label"
+                        v-bind="bar.label"
+                        :styles="{ ...labelStylesCmp, transition, transform: `translateY(${bar.label.y}px)` }"
+                        :align="labelAlign"
+                        :size="labelSize"
+                    >
+                        {{ bar.label.value }}
+                        </text>
+                    </slot>
                     <text
-                        v-if="bar.label"
                         :x="bar.label.x"
                         :y="0"
                         :text-anchor="labelAlign"
                         :font-size="labelSize"
                         :style="{ ...labelStylesCmp, transition, transform: `translateY(${bar.label.y}px)` }"
                     >
-                        {{ bar.label.value }}
+                        <slot
+                            name="labelValue"
+                            v-bind="bar.label"
+                        >
+                            {{ bar.label.value }}
+                        </slot>
                     </text>
-                </slot>
+                    </slot>
+                </g>
+                <g
+                    v-if="bar.stackedLabel"
+                >
+                    <slot
+                        name="stackedLabel"
+                        v-bind="bar.stackedLabel"
+                        :styles="{ ...labelStylesCmp, transition, transform: `translateY(${bar.stackedLabel.y}px)` }"
+                        :align="labelAlign"
+                        :size="labelSize"
+                    >
+                        <text
+                            v-if="bar.stackedLabel"
+                            :x="bar.stackedLabel.x"
+                            :y="0"
+                            :text-anchor="labelAlign"
+                            :font-size="labelSize"
+                            :style="{ ...labelStylesCmp, transition, transform: `translateY(${bar.stackedLabel.y}px)` }"
+                        >
+                            <slot
+                                name="stackedLabelValue"
+                                v-bind="bar.stackedLabel"
+                            >
+                                {{ bar.stackedLabel.stackedValue }}
+                            </slot>
+                        </text>
+                    </slot>
+                </g>
             </g>
         </WTrans>
     </g>
@@ -69,6 +110,7 @@ export default {
         datakey: VueTypes.string.isRequired,
         legend: VueTypes.string, // Prop to apply filters
         showLabel: VueTypes.bool.def(false),
+        showStackedLabel: VueTypes.bool.def(false),
         labelSize: VueTypes.number.def(12),
         labelAlign: VueTypes.oneOf(['start', 'middle', 'end']).def('middle'),
         labelPosition: VueTypes.oneOf(['inside', 'outside']).def('outside'),
@@ -139,7 +181,8 @@ export default {
             // Generate points array
             return data.map((value, i) => {
                 let [start, end] = value
-                const label = end
+                const label = end - start
+                const stackedValue = end
 
                 // If start value is negative, reverse values
                 if (start < 0) {
@@ -156,20 +199,23 @@ export default {
                 // Calc xAxis pos
                 const x = (x0 + (space * i) + padding[3])
 
-                return [x, y, y0, label]
+                return [x, y, y0, label, stackedValue]
             })
         },
         // Bars
         bars () {
             // Generate bars array
             return this.points.map((point, index) => {
-                const [x0, y0, y1, value] = point
+                const [x0, y0, y1, value, stackedValue] = point
                 // Generate coords
                 const height = y1 - y0
                 const x = x0 + this.margin
                 const y = height < 0 ? y1 : y0
                 const label = this.getLabel({
                     x, y, value, height,
+                })
+                const stackedLabel = this.getStackedLabel({
+                    x, y, stackedValue, height,
                 })
 
                 return {
@@ -178,6 +224,7 @@ export default {
                     width: this.width,
                     height: Math.abs(height),
                     label,
+                    stackedLabel,
                 }
             })
         },
@@ -195,8 +242,6 @@ export default {
             x, y, value, height,
         }) {
             if (!this.showLabel) return undefined
-            // If has stacked bars, only last bar shown the label
-            if (this.Chart.stacked && this.id !== this.getLastBarActive()) return undefined
 
             // Calc position of label [x, y]
             const top = (this.labelPosition === 'outside' ? this.labelSize : -(this.labelSize))
@@ -207,6 +252,31 @@ export default {
                 x: x0,
                 y: y1,
                 value,
+            }
+        },
+        getStackedLabel ({
+            x, y, stackedValue, height,
+        }) {
+            if (!this.showStackedLabel) return undefined
+
+            // Only render if is stacked
+            if (!this.Chart.stacked) return undefined
+
+            // Only last bar shown the stacked label
+            if (this.id !== this.getLastBarActive()) return undefined
+
+            // If label is printed outside, not render staked label
+            if (this.showLabel && this.labelPosition === 'outside') return undefined
+
+            // Calc position of label [x, y]
+            const top = this.labelSize
+            const x0 = x + this.width / 2
+            const y1 = y - top + this.labelSize / 2
+
+            return {
+                x: x0,
+                y: y1,
+                stackedValue,
             }
         },
         // Set active element
