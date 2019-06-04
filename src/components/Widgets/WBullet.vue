c7d29e434681b1b0057192ceaddc3f39b6d07931
alejandroledesma <alexledesmarayo@gmail.com>
2019-06-04 12:07:32 +0200

    Update margin in WBullet

--- src/components/Widgets/WBullet.vue
+++ src/components/Widgets/WBullet.vue
@@ -23,7 +23,7 @@ export default {
         width: VueTypes.oneOfType([String, Number]).def(10),
         height: VueTypes.oneOfType([String, Number]).def(10),
         borderRadius: VueTypes.oneOfType([String, Number]).def(50),
-        margin: VueTypes.oneOfType([VueTypes.number, VueTypes.arrayOf(VueTypes.number).def([0, 0, 0, 0])]).def(8),
+        margin: VueTypes.oneOfType([VueTypes.number, VueTypes.arrayOf(VueTypes.number).def([0, 0, 0, 0])]).def([0, 8, 0, 0]),
     },
 }
 </script>
