5dce8c1ca9e9addb0c0bd800370d38eb21d4f552
alejandroledesma <alexledesmarayo@gmail.com>
2019-07-12 11:25:59 +0200

    Fix WShowIfFit component

--- src/components/Widgets/WShowIfFit/WShowIfFit.spec.js
+++ src/components/Widgets/WShowIfFit/WShowIfFit.spec.js
@@ -3,20 +3,6 @@ import WShowIfFit from './WShowIfFit.vue'
 
 describe('Widgets/WShowIfFit', () => {
     const propsData = {}
-    const Parent = {
-        name: 'parent',
-        components: {
-            WShowIfFit,
-        },
-        template: `
-            <div id="test" style="width: 25px; height: 50px;">
-                <WShowIfFit id="component">
-                    <div id="child" style="width: 50px; height: 25px;">
-                    </div>
-                </WShowIfFit>
-            </div>
-        `,
-    }
     const defaultConfig = {
         propsData,
         slots: {
@@ -25,7 +11,36 @@ describe('Widgets/WShowIfFit', () => {
     }
 
     it(`Should be render correctly`, () => {
-        const wrapper = shallowMount(Parent, defaultConfig)
+        const wrapper = shallowMount(WShowIfFit, defaultConfig)
         expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it(`Should be check if show or hide child component`, () => {
+        const wrapper = shallowMount(WShowIfFit, defaultConfig)
+
+        Object.defineProperty(wrapper.vm.$el, 'parentNode', {
+            get () {
+                return {
+                    appendChild: () => undefined,
+                    removeChild: () => undefined,
+                    clientWidth: 25,
+                }
+            },
+        })
+
+        Object.defineProperty(wrapper.vm.$el, 'children', {
+            get () {
+                return [
+                    {
+                        appendChild: () => undefined,
+                        removeChild: () => undefined,
+                        offsetWidth: 50,
+                    },
+                ]
+            },
+        })
+
+        wrapper.vm.handleSize()
+        expect(wrapper.vm.hide).toBeTruthy()
     })
 })
