4ec2ef900a7e4d3dbdbe75ae753d366f1c922d46
Jaime Serrano Hernández <jaimeserranohdez@gmail.com>
2019-08-08 11:17:41 +0200

    Marker test is testing x type now.

--- src/components/Marker/WMarker.spec.js
+++ src/components/Marker/WMarker.spec.js
@@ -2,6 +2,18 @@ import { shallowMount } from '@vue/test-utils'
 import WMarker from './WMarker.vue'
 
 describe('Components/WMarker', () => {
+    const dataset = [
+        {
+            name: 'Page A', one: 4000, two: -2400, three: 2300, four: 1200, five: 2300,
+        },
+        {
+            name: 'Page B', one: 3000, two: 2210, three: 1398, four: 1200, five: 1398,
+        },
+        {
+            name: 'Page C', one: 9800, two: 2200, three: 0, four: 1200, five: 0,
+        },
+    ]
+
     const propsData = {
         value: {
             range: true,
@@ -12,8 +24,11 @@ describe('Components/WMarker', () => {
 
     const provide = {
         Chart: {
+            dataset,
+            axis: { x: { datakey: 'name', name: '' } },
             legends: ['One Bar', 'Two Bar'],
             space: [20, 20, 20, 20],
+            padding: [20, 20, 20, 20],
             activeElements: [0, 1],
             yScale: a => a,
             canvas: {
@@ -34,6 +49,17 @@ describe('Components/WMarker', () => {
 
     it(`Should be render correctly`, () => {
         const wrapper = shallowMount(WMarker, defaultConfig)
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it(`Should be render correctly with x type`, () => {
+        const wrapper = shallowMount(WMarker, {
+            ...defaultConfig,
+            propsData: {
+                ...defaultConfig.propsData,
+                type: 'x',
+            },
+        })
         expect(wrapper.html()).toMatchSnapshot()
     })
 })
