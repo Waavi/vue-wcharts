065f9d3babd21d2bbe9a5624281d36475352cbb0
Jaime Serrano Hernández <jaimeserranohdez@gmail.com>
2019-08-14 12:36:04 +0200

    Widgets.

---
+++ docs/.vuepress/components/Examples/Widgets/LegendCustom.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <WLegendItem
+            v-for="(legend, index) in legends"
+            :key="index"
+            :index="index"
+            :text="legend"
+            :style="{ marginBottom: '10px' }"
+            active
+        >
+            <template #bullet="{ color }">
+                <span :style="{ fontSize: '18', marginRight: '10px', color }">☎</span>
+            </template>
+        </WLegendItem>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LegendCustom',
+    data () {
+        return {
+            legends: [
+                'Corrective costs',
+                'Preventive costs',
+                'Future costs',
+            ],
+        }
+    }
+}
+</script>
---
+++ docs/examples/widgets.md
@@ -0,0 +1,35 @@
+---
+sidebarDepth: 1
+---
+# Widgets
+
+## Legend Item
+
+<br>
+
+<Examples-Widgets-LegendItem />
+
+<SourceCode>
+<<< @/docs/.vuepress/components/Examples/Widgets/LegendItem.vue
+</SourceCode>
+
+## Legend
+
+<br>
+
+<Examples-Widgets-Legend />
+
+<SourceCode>
+<<< @/docs/.vuepress/components/Examples/Widgets/Legend.vue
+</SourceCode>
+
+## Legend Custom
+
+<br>
+
+<Examples-Widgets-LegendCustom />
+
+<SourceCode>
+<<< @/docs/.vuepress/components/Examples/Widgets/LegendCustom.vue
+</SourceCode>
+
