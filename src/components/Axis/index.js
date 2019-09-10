f13b3724dd31aade19e740a719451cacf66238a8
alejandroledesma <alexledesmarayo@gmail.com>
2019-09-10 09:21:29 +0200

    Create WZAxis folder

--- src/components/Axis/index.js
+++ src/components/Axis/index.js
@@ -1,3 +1,3 @@
 export { default as WXAxis } from './WXAxis/WXAxis.vue'
 export { default as WYAxis } from './WYAxis/WYAxis.vue'
-export { default as WZAxis } from './WZAxis.vue'
+export { default as WZAxis } from './WZAxis/WZAxis.vue'
