c1967c1469702b72b5eb48565f1dd6a60a2772d3
alejandroledesma <alejandro.ledesma.dev@gmail.com>
2019-11-25 15:12:15 +0100

    Update tests

--- src/components/Pie/WGauge/WGauge.spec.js
+++ src/components/Pie/WGauge/WGauge.spec.js
@@ -1,7 +1,7 @@
-import { shallowMount, mount } from '@vue/test-utils'
-import WPie from './WPie.vue'
+import { shallowMount } from '@vue/test-utils'
+import WGauge from './WGauge.vue'
 
-describe('Components/WPie', () => {
+describe('Components/WGauge', () => {
     const propsData = {
         datakey: 'one',
         animation: false,
@@ -9,25 +9,7 @@ describe('Components/WPie', () => {
     }
     const dataset = [
         {
-            name: 'Page A', one: 1000, two: 2400, three: 2400,
-        },
-        {
-            name: 'Page B', one: 3000, two: 1398, three: 2210,
-        },
-        {
-            name: 'Page C', one: 2000, two: 9800, three: 2290,
-        },
-        {
-            name: 'Page D', one: 2780, two: 3908, three: 2000,
-        },
-        {
-            name: 'Page E', one: 1890, two: 0, three: 1700,
-        },
-        {
-            name: 'Page F', one: 2390, two: 3800, three: 2500,
-        },
-        {
-            name: 'Page G', one: 3490, two: 4300, three: -1000,
+            name: 'Page A', one: 70, total: 100,
         },
     ]
 
@@ -57,52 +39,7 @@ describe('Components/WPie', () => {
     }
 
     it(`Should be render correctly`, () => {
-        const wrapper = shallowMount(WPie, defaultConfig)
+        const wrapper = shallowMount(WGauge, defaultConfig)
         expect(wrapper.html()).toMatchSnapshot()
-    })
-
-    it('Should be render correctly with custom styles', () => {
-        const wrapper = mount(WPie, { ...defaultConfig, propsData: { ...propsData, styles: { backgroundColor: 'green' } } })
-        expect(wrapper.vm.stylesCmp).toEqual({ backgroundColor: 'green', position: 'relative', transform: 'translate(50%, 50%)' })
-    })
-
-    it('Should be render correctly with custom radius', () => {
-        const wrapper = mount(WPie, { ...defaultConfig, propsData: { ...propsData, radius: [150, 180] } })
-        expect(wrapper.vm.curRadius).toEqual({ innerRadius: 150, outerRadius: 180 })
-        expect(wrapper.html()).toMatchSnapshot()
-
-        const wrapperTwo = mount(WPie, { ...defaultConfig, propsData: { ...propsData, radius: null } })
-        expect(wrapperTwo.vm.curRadius).toEqual({ innerRadius: 0, outerRadius: 100 })
-    })
-
-    it('It emits the handleClick event', () => {
-        const wrapper = mount(WPie, { ...defaultConfig, propsData: { ...propsData, trigger: 'click' } })
-        wrapper.findAll('path').at(0).trigger('click')
-        expect(wrapper.emitted('onClick')).toHaveLength(1)
-    })
-
-    it('It emits the handleMouseEnter event', () => {
-        const wrapper = mount(WPie, defaultConfig)
-        wrapper.findAll('path').at(0).trigger('mouseenter')
-        expect(wrapper.emitted('onMouseenter')).toHaveLength(1)
-    })
-
-    it('It emits the handleClick event', () => {
-        const wrapper = mount(WPie, { ...defaultConfig, propsData: { ...propsData, trigger: 'click' } })
-        wrapper.findAll('path').at(0).trigger('click')
-        expect(wrapper.emitted('onClick')).toHaveLength(1)
-    })
-
-    it('It emits the handleMouseleave event', () => {
-        const wrapper = mount(WPie, { ...defaultConfig, propsData: { ...propsData, trigger: 'hover' } })
-        wrapper.findAll('path').at(0).trigger('mouseleave')
-        expect(wrapper.emitted('onMouseleave')).toHaveLength(1)
-    })
-
-    it('Should be active 1', () => {
-        const wrapper = mount(WPie, defaultConfig)
-        wrapper.setProps({ itemActive: 1 })
-        expect(wrapper.props('itemActive')).toBe(1)
-        expect(wrapper.vm.activePath).toBe(1)
     })
 })
